bf349011011704c96f8d968e40b489fa4bd2825d
d.mathisen <drew.a.mathisen@gmail.com>
2021-12-06 19:47:57 -0700

    update route experience

--- app/controllers/experiences_controller.rb
+++ app/controllers/experiences_controller.rb
@@ -28,4 +28,22 @@ class ExperiencesController < ApplicationController
     render json: e
   end
 
+  def update
+    the_id = params[:id]
+    e = Experience.find_by(id: the_id) 
+    e.start_date = params[:start_date] || e.start_date
+    e.end_date = params[:end_date] || e.end_date
+    e.job_title = params[:job_title] || e.job_title
+    e.company_name = params[:company_name] || e.company_name
+    e.details = params[:details] || e.details
+    e.student_id =  e.student_id
+
+    if e.save
+      render json: e
+    else 
+      render json: e.errors.full_messages
+    end
+  end
+  
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get "/experiences" => "experiences#index"
   post "/experiences" => "experiences#create"
   get "/experiences/:id" => "experiences#show" 
+  patch "/experiences/:id" => "experiences#update"
   
 end
 
