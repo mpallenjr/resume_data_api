146df0970e358d90d7832fb5533848d2d5fbc97b
britneykline <87348224+britneykline@users.noreply.github.com>
2021-12-06 20:53:58 -0500

    create capstone model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_07_011849) do
+ActiveRecord::Schema.define(version: 2021_12_07_014846) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "capstones", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.string "url"
+    t.string "screenshot"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.integer "student_id"
+  end
 
   create_table "students", force: :cascade do |t|
     t.string "first_name"
