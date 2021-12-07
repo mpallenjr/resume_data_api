1588006c751fa2e50b1191223f9c7656dee3313a
britneykline <87348224+britneykline@users.noreply.github.com>
2021-12-06 21:42:02 -0500

    add create action for students

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,2 +1,20 @@
 class StudentsController < ApplicationController
+  def create
+    student = Student.new(
+      first_name: params[:first_name], 
+      last_name: params[:last_name],
+      email: params[:email], 
+      phone_number: params[:phone_number],
+      bio: params[:bio],
+      linkedin_url: params[:linkedin_url],
+      twitter_handle: params[:twitter_handle],
+      website_url: params[:website_url],
+      resume_url: params[:resume_url],
+      github_url: params[:github_url],
+      photo: params[:photo],
+      password: params[:password]
+    )
+    student.save
+    render json: student.as_json
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "/experiences" => "experiences#index"
   post "/experiences" => "experiences#create"
+  post "/students" => "students#create"
 end
 
