75b8811d80c888247853c3cc9ee036a314067b77
d.mathisen <drew.a.mathisen@gmail.com>
2021-12-06 19:34:49 -0700

    finished index and create actions

--- app/controllers/experiences_controller.rb
+++ app/controllers/experiences_controller.rb
@@ -5,7 +5,26 @@ class ExperiencesController < ApplicationController
   end
 
   def create
-    c = "in create"
-    render json: c
+    e = Experience.new(
+      start_date: params[:start_date],
+      end_date: params[:end_date],
+      job_title: params[:job_title],
+      company_name: params[:company_name],
+      details: params[:details],
+      student_id: params[:student_id]
+    )
+
+    if e.save
+      render json: e
+    else
+      render json: e.errors.full_messages
+    end
+
   end
+
+  def show
+    e = "in show"
+    render json: e
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # experiences routes
   get "/experiences" => "experiences#index"
   post "/experiences" => "experiences#create"
+  get "/experiences/:id" => "experiences#show" 
+  
 end
 
